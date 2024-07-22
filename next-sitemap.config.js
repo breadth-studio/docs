0c06abf223698bb63742f4099ab93fd1d8747891
kentio <13550898+kentio@users.noreply.github.com>
2024-07-22 18:43:39 +0800

    feat: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://breadth.app/docs',
+    generateRobotsTxt: true, // (optional)
+    robotsTxtOptions: {
+        policies: [{ userAgent: '*', disallow: "/"}],
+    },
+}
